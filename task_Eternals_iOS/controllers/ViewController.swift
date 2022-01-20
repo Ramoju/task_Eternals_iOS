b7d278d6904d0c325a27d3d1143b6263845abba9
c0834351 <c0834351@mylambton.ca>
2022-01-20 01:21:55 -0500

    alert-in-first-view

--- task_Eternals_iOS/controllers/ViewController.swift
+++ task_Eternals_iOS/controllers/ViewController.swift
@@ -12,22 +12,48 @@ class ViewController: UIViewController {
     
     var categories = ["Books", "Groceries", "Shopping-List", "Movies"]
     @IBOutlet weak var categoryTv: UITableView!
-    @IBOutlet weak var addCatBtn: UIButton!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         categoryTv.delegate=self
         categoryTv.dataSource=self
-        
+        categoryTv.reloadData()
     }
-
-
-    @IBAction func addCategory(_ sender: UIButton) {
-        
-        
+    
+    @IBAction func addCategory() {
+        showAlert()
     }
-}
+    
+   
+    func showAlert(){
+        let alert = UIAlertController(title: "Alert!", message: "Please enter the Category Name", preferredStyle: .alert)
+        
+        //adding text field
+        alert.addTextField { field in
+            field.placeholder = "category name"
+            field.returnKeyType = .continue
+            field.keyboardType = .emailAddress
+        }
+        
+        //adding two buttons to alert
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { [self] _ in
+            // read the textfields from alert box
+            guard let fields = alert.textFields, fields.count == 1 else{
+                return
+            }
+            let catName = fields[0]
+            guard let name = catName.text, !name.isEmpty else{
+                return
+            }
+            self.categories.append(name)
+            categoryTv.reloadData()
+        }))
+        present(alert, animated: true)
+    }
+    }
+    
+  
 
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
